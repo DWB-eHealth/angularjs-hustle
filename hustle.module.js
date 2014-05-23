f6666e534951d7eff1a239a5a7f4793b160b2de7
Aravind M <arav.psgtech@gmail.com>
2014-05-23 10:44:01 +0530

    k1,araving | 102 | hustle angular provider

---
+++ hustle.module.js
@@ -0,0 +1,84 @@
+(function(angular) {
+    angular.module('hustle', []).provider('$hustle', function() {
+        var self = this;
+        var hustle;
+        var $q;
+
+        var getHustle = function() {
+            var deferred = $q.defer();
+            if (!hustle) {
+                deferred.reject("Please call init on provider first.");
+            } else if (hustle.is_open()) {
+                deferred.resolve(hustle);
+            } else {
+                hustle.open({
+                    "success": function(e) {
+                        deferred.resolve();
+                    },
+                    "error": function(e) {
+                        deferred.reject(e);
+                    }
+                });
+            }
+            return deferred.promise;
+        };
+
+        var publishMessage = function(message, tube) {
+            var deferred = $q.defer();
+            hustle.Queue.put(message, {
+                "tube": tube,
+                "success": function(item) {
+                    deferred.resolve(item.id);
+                },
+                "error": function(e) {
+                    deferred.reject(e);
+                }
+            });
+            return deferred.promise;
+        };
+
+        var subscribeTo = function(callback, tube, delay) {
+            var callCallbackAndDeleteItemFromQ = function(job) {
+                callback.call(this, job);
+                hustle.Queue.delete(job.id);
+            };
+
+            return new hustle.Queue.Consumer(callCallbackAndDeleteItemFromQ, {
+                "tube": tube,
+                "delay": delay
+            });
+        };
+
+        self.init = function(db_name, db_version, tubes) {
+            hustle = new Hustle({
+                "db_name": db_name,
+                "db_version": db_version,
+                "tubes": tubes
+            });
+        };
+
+        self.$get = ['$q', '$rootScope',
+            function(q, $rootScope) {
+                $q = q;
+
+                var publish = function(message, tube) {
+                    return getHustle().then(function() {
+                        publishMessage(message, tube);
+                    });
+                };
+
+                var subscribe = function(callback, tube, delay) {
+                    return getHustle().then(function() {
+                        return subscribeTo(callback, tube, delay);
+                    });
+                };
+
+                return {
+                    "publish": publish,
+                    "subscribe": subscribe
+                };
+            }
+        ];
+
+    });
+}(angular));
---
+++ test/hustle.module.spec.js
@@ -0,0 +1,3 @@
+describe("hustle angular provider", function() {
+
+});
